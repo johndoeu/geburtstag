c86bcb4ecd396bc2f2771f909c752142e8ff016a
johndoeu <131233002+johndoeu@users.noreply.github.com>
2023-04-20 21:11:38 +0700

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = (): JSX.Element => {
   return (
     <nav>
       <Link href="/">
-        <a className="text-gray-900 dark:text-white pr-6 py-4">It's Me</a>
+        <a className="text-gray-900 dark:text-white pr-6 py-4">It&apos;s Me</a>
       </Link>
       <Link href="/about">
         <a className="text-gray-900 dark:text-white px-6 py-4">The Story</a>
